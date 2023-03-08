d482f0d6666b2d636f97bb228ff5b7ef8f6e3882
Yu Li <liyu1981@gmail.com>
2023-03-08 08:55:43 +0000

    1st version, async requests

--- lib/blitzy.ex
+++ lib/blitzy.ex
@@ -1,18 +1,44 @@
 defmodule Blitzy do
-  @moduledoc """
-  Documentation for `Blitzy`.
-  """
+  use Application
 
-  @doc """
-  Hello world.
+  def start(_type, _args) do
+    Supervisor.start_link([], strategy: :one_for_one)
+  end
 
-  ## Examples
+  def run(n_workers, url) when n_workers > 0 do
+    worker_func = fn -> Blitzy.Worker.start(url) end
 
-      iex> Blitzy.hello()
-      :world
+    1..n_workers
+    |> Enum.map(fn _ -> Task.async(worker_func) end)
+    |> Enum.map(&Task.await(&1, :infinity))
+  end
 
-  """
-  def hello do
-    :world
+  def parse_results(results) do
+    {successes, _failures} =
+      results
+      |> Enum.split_with(fn x ->
+        case x do
+          {:ok, _} -> true
+          _ -> false
+        end
+      end)
+
+    total_workers = Enum.count(results)
+    total_success = Enum.count(successes)
+    total_failure = total_workers - total_success
+
+    data = successes |> Enum.map(fn {:ok, time} -> time end)
+    average_time = Enum.sum(data) / Enum.count(data)
+    longest_time = Enum.max(data)
+    shortest_time = Enum.min(data)
+
+    IO.puts("""
+    Total workers   : #{total_workers}
+    Successful reqs : #{total_success}
+    Failed reqs     : #{total_failure}
+    Average (msecs) : #{average_time}
+    Longest (msecs) : #{longest_time}
+    Shortest (msecs): #{shortest_time}
+    """)
   end
 end
--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule Blitzy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Blitzy, []},
       extra_applications: [:logger]
     ]
   end
@@ -21,6 +22,9 @@ defmodule Blitzy.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:httpoison, "~> 2.0.0"},
+      {:timex, "~> 3.0"},
+      {:tzdata, "~> 0.1.8", override: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
